657acee0e36b6ada7cf03bb8661036c19d75325a
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2022-05-15 15:28:20 +0530

    Minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
+import re
+from os import listdir
+from os.path import isfile, join
+
 import streamlit as st
 import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+
+files = [f for f in listdir("ClimateChangeModified") if isfile(join("ClimateChangeModified", f))]
+
+dataFrame = None
+
+for f in files:
+    if re.search(".csv$", f):
+        dataFrame = pd.read_csv("ClimateChangeModified/" + f, names=["date", "death"])
+
+if dataFrame is None:
+    raise Exception("Issue with reading file")
+
+st.write(dataFrame)
