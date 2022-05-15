b237cd193cde8c9c8afcfe2dc755c55e9f1dde40
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2022-05-15 23:08:20 +0530

    Minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,14 @@ import pandas as pd
 import datetime
 
 st.write("Monthly Deaths Caused By Covid-19")
-
-files = [f for f in listdir("ClimateChangeModified") if isfile(join("ClimateChangeModified", f))]
+working_directory = "CovidModified"
+files = [f for f in listdir(working_directory) if isfile(join(working_directory, f))]
 
 df = None
 
 for f in files:
     if re.search(".csv$", f):
-        df = pd.read_csv("ClimateChangeModified/" + f, names=["date", "death"])
+        df = pd.read_csv(working_directory + "/" + f, names=["date", "death"])
 
 if df is None:
     raise Exception("Issue with reading file")
